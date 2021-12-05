c727729648d2952d7f9f5de02fc83f0b2e2810ab
nvershinin <vershinin_nikita@mail.ru>
2021-12-05 21:06:28 +0300

    homework3 updated

--- homework3/js/task1.js
+++ homework3/js/task1.js
@@ -1,72 +1,49 @@
 "use strict";
 
-let services =  [
+let articles =  [
     {
-        title: "Женская стрижка на длинные волосы",
-        price: 2000
+        title: "Нейросеть может читать текст быстрее человека",
+        image: "https://picsum.photos/id/22/1000/1000"
     },
     {
-        title: "Стрижка челки",
-        price: 300
+        title: "Пять сервисов, которые помогут написать статью за 5 минут",
+        image: "https://picsum.photos/id/24/1000/1000"
     },
     {
-        title: "Детская стрижка (до 7 лет)",
-        price: 700
+        title: "Названы основные правила движения поездов",
+        image: "https://picsum.photos/id/28/1000/1000"
     },
     {
-        title: "Мужская стрижка",
-        price: 1000
+        title: "Самая посещаемая достопримечательность мира",
+        image: "https://picsum.photos/id/27/1000/1000"
     },
     {
-        title: "Коррекция бороды",
-        price: 1200
+        title: "Появился новый термин для обозначения радиоволн",
+        image: "https://picsum.photos/id/29/1000/1000"
     },
     {
-        title: "Тонирование мужских волос",
-        price: 1500
-    },
-    {
-        title: "Окрашивание волос",
-        price: 5000
-    },
-    {
-        title: "Женский маникюр",
-        price: 1500
-    },
-    {
-        title: "Мужской маникюр",
-        price: 1700
-    },
-    {
-        title: "Педикюр",
-        price: 2500
-    },
-    {
-        title: "Покрытие гель-лаком",
-        price: 1500
+        title: "В России появится новый вид бумаги",
+        image: "https://picsum.photos/id/30/1000/1000"
     }
 ];
 
 let app = Vue.createApp({
     data(){
         return {
-            services,
-            selectedServices: [], // простой массив, хранящий индексы услуг из services
+            articles,
+            input: "",
         }
     },
     computed: {
-        finalPrice(){
-            let result = 0;
-            for (let service of this.selectedServices) {
-                result += this.services[service].price
-            }
-            return result;
+        matchingArticles(){
+            return this.articles.filter(article => {
+                return article.title.toLowerCase().includes(this.input.toLowerCase())
+            })
         }
     },
     methods: {
 
-    },
-
+    }
 });
 
 app.mount("#app");
